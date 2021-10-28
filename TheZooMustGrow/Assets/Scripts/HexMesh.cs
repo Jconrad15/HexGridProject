e3ba9d06d6bc74d22b32d2b81a1bba6a91124f47
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-10-27 18:03:21 -0700

    Triangulating blend regions

--- TheZooMustGrow/Assets/Scripts/HexMesh.cs
+++ TheZooMustGrow/Assets/Scripts/HexMesh.cs
@@ -56,17 +56,26 @@ namespace TheZooMustGrow
         void Triangulate(HexDirection direction, HexCell cell)
         {
             Vector3 center = cell.transform.localPosition;
-            AddTriangle(
-                center,
-                center + HexMetrics.GetFirstCorner(direction),
-                center + HexMetrics.GetSecondCorner(direction)
-            );
-            
+            Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+            Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+            AddTriangle(center, v1, v2);
+
+            // Solid color for the triangle
+            AddTriangleColor(cell.color);
+
+            Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
+            Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
+
+            AddQuad(v1, v2, v3, v4);
+
             HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
             HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
             HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
 
-            AddTriangleColor(
+            // Blended colors for the quad
+            AddQuadColor(
+                cell.color,
                 cell.color,
                 (cell.color + prevNeighbor.color + neighbor.color) / 3f,
                 (cell.color + neighbor.color + nextNeighbor.color) / 3f
@@ -108,6 +117,32 @@ namespace TheZooMustGrow
             colors.Add(c1);
             colors.Add(c2);
             colors.Add(c3);
+        }
+
+        void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+        {
+            int vertexIndex = vertices.Count;
+
+            vertices.Add(v1);
+            vertices.Add(v2);
+            vertices.Add(v3);
+            vertices.Add(v4);
+
+            // two triangles in the quad
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 3);
+        }
+
+        void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
+        {
+            colors.Add(c1);
+            colors.Add(c2);
+            colors.Add(c3);
+            colors.Add(c4);
         }
 
     }
