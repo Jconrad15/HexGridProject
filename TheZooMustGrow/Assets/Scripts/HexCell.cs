2daa5b09cce8a84b65d84cefb2d5030eb3311a4a
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-10-27 17:27:26 -0700

    Cell neighbors

--- TheZooMustGrow/Assets/Scripts/HexCell.cs
+++ TheZooMustGrow/Assets/Scripts/HexCell.cs
@@ -7,5 +7,29 @@ namespace TheZooMustGrow
         public HexCoordinates coordinates;
 
         public Color color;
+
+        [SerializeField]
+        private HexCell[] neighbors;
+
+        /// <summary>
+        /// Returns the neighboring HexCell in the provided direction.
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public HexCell GetNeighbor(HexDirection direction)
+        {
+            return neighbors[(int)direction];
+        }
+
+        /// <summary>
+        /// Sets the neighboring Hexcell in the provided direction.
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <param name="cell"></param>
+        public void SetNeighbor(HexDirection direction, HexCell cell)
+        {
+            neighbors[(int)direction] = cell;
+            cell.neighbors[(int)direction.Opposite()] = this;
+        }
     }
 }
