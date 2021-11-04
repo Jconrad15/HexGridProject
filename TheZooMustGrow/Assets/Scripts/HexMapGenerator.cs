496efa72a72a97cc5c09392def1e84cc9e426edc
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-11-04 15:50:07 -0700

    Creating land

--- TheZooMustGrow/Assets/Scripts/HexMapGenerator.cs
+++ TheZooMustGrow/Assets/Scripts/HexMapGenerator.cs
@@ -5,24 +5,107 @@ namespace TheZooMustGrow
 {
 	public class HexMapGenerator : MonoBehaviour
 	{
-
 		public HexGrid grid;
+		private int cellCount;
 
+		HexCellPriorityQueue searchFrontier;
+		int searchFrontierPhase;
+
+		[Range(0f, 0.5f)]
+		public float jitterProbability = 0.25f;
+
+		[Range(20, 200)]
+		public int chunkSizeMin = 30;
+
+		[Range(20, 200)]
+		public int chunkSizeMax = 100;
+
+		[Range(5, 95)]
+		public int landPercentage = 50;
 
 		public void GenerateMap(int x, int z)
 		{
+			cellCount = x * z;
+
 			// Generate base map
 			grid.CreateMap(x, z);
 
-            for (int i = 0; i < z; i++)
+            // Initiate search frontier
+            if (searchFrontier == null)
             {
-				grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+				searchFrontier = new HexCellPriorityQueue();
             }
 
+			CreateLand();
 
+            // Set all search phase variables in cells to zero
+            for (int i = 0; i < cellCount; i++)
+            {
+				grid.GetCell(i).SearchPhase = 0;
+            }
 		}
 
+		private void CreateLand()
+        {
+            int landBudget = Mathf.RoundToInt(cellCount * landPercentage * 0.01f);
 
+			while (landBudget > 0)
+            {
+				landBudget = RaiseTerrain(
+					Random.Range(chunkSizeMin, chunkSizeMax + 1), landBudget);
+            }
+        }
+
+		private int RaiseTerrain(int chunkSize, int budget)
+        {
+			// Start search for random group of terrain
+			searchFrontierPhase += 1;
+			HexCell firstCell = GetRandomCell();
+			firstCell.SearchPhase = searchFrontierPhase;
+			firstCell.Distance = 0;
+			firstCell.SearchHeuristic = 0;
+			searchFrontier.Enqueue(firstCell);
+
+			HexCoordinates center = firstCell.coordinates;
+
+			int size = 0;
+			while (size < chunkSize && searchFrontier.Count > 0)
+            {
+				HexCell current = searchFrontier.Dequeue();
+
+				// If the current cell has not yet been raised
+				if (current.TerrainTypeIndex == 0)
+                {
+					// Check if the budget has been reached
+					current.TerrainTypeIndex = 1;
+					if (--budget == 0)
+                    {
+						break;
+                    }
+				}
+				size += 1;
+
+				for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
+				{
+					HexCell neighbor = current.GetNeighbor(d);
+					if(neighbor && neighbor.SearchPhase < searchFrontierPhase)
+                    {
+						neighbor.SearchPhase = searchFrontierPhase;
+						neighbor.Distance = neighbor.coordinates.DistanceTo(center);
+						neighbor.SearchHeuristic = Random.value < jitterProbability ? 1 : 0;
+						searchFrontier.Enqueue(neighbor);
+                    }
+
+				}
+            }
+			searchFrontier.Clear();
+			return budget;
+        }
+
+		private HexCell GetRandomCell()
+        {
+			return grid.GetCell(Random.Range(0, cellCount));
+        }
 
 	}
 }
