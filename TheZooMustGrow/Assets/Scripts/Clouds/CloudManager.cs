e5fd6d950835eda4db06eef3e16f386f6b046fb0
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-11-11 15:11:21 -0800

    Start clouds

---
+++ TheZooMustGrow/Assets/Scripts/Clouds/CloudManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheZooMustGrow
+{
+    public class CloudManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
